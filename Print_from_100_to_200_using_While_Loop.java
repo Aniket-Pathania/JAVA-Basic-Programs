78f159ee153d56d702194821e8f9716c35ca6c73
Aniket-Pathania <aniketpathania313@gmail.com>
2026-01-26 14:32:54 +0530

    Add files via upload

---
+++ Print_from_100_to_200_using_While_Loop.java
@@ -0,0 +1,14 @@
+package com.company;
+public class Print_from_100_to_200_using_While_Loop {
+    public static void main(String[] args){
+        System.out.println("Print from 100 to 200 ");
+        int Start = 100;
+        while (Start<=200){
+
+            System.out.println(Start);
+            Start += 1;
+
+        }
+        System.out.println("The numbers are listed from 100 to 200");
+    }
+}
