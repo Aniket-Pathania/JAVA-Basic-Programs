b5b2a1c12662bc040b7e2af81055ed3d36662a69
Aniket-Pathania <aniketpathania313@gmail.com>
2026-01-27 19:19:05 +0530

    Add files via upload

---
+++ DO_While_Loop_Concept_code.java
@@ -0,0 +1,12 @@
+package com.company;
+public class DO_While_Loop_Concept_code {
+    public static void main(String[] args){
+        System.out.println("Print numbers from 1 to 45 using Do while");
+        int a = 1;
+        do {
+            System.out.println(a);
+            a+=1;
+        }while (a<=45);
+        System.out.println("End");
+    }
+}
