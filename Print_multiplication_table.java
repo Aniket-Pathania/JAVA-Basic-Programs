2129178ca8f29460242b5c7e402695d2916f980e
Aniket-Pathania <aniketpathania313@gmail.com>
2026-02-01 18:15:25 +0530

    Add files via upload

---
+++ Print_multiplication_table.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Print_multiplication_table {
+    public static void main(String[] args){
+        System.out.println("Printing multiplication table of 5");
+        for (int i = 0;i<11;i+=1){
+            System.out.println(5 + " * " + i + " = " + 5*i);
+        }
+    }
+}
